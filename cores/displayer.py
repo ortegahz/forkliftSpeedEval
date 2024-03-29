319a4a1550ad6edc060cafa181e2fe333c84890a
manu <zxthz@126.com>
2024-03-29 12:54:50 +0800

    add trajectories plot

--- cores/displayer.py
+++ cores/displayer.py
@@ -1,5 +1,6 @@
-import cv2
 import time
+
+import cv2
 import numpy as np
 
 
@@ -13,6 +14,8 @@ def process_displayer(max_track_num, queue, event):
     start_time = time.time()
     frame_count = 0
 
+    trajectories_dict = dict()
+
     while True:
         idx_frame, frame, online_targets = queue.get()
         for track in online_targets:
@@ -25,17 +28,23 @@ def process_displayer(max_track_num, queue, event):
             sort_id = box[4].astype(np.int32)
             color_id = sort_colours[sort_id % max_track_num, :]
             cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), color_id, 2)
+            point_center = (int((box[0] + box[2]) / 2), int((box[1] + box[3]) / 2))
+            if tid in trajectories_dict:
+                trajectories_dict[tid].append(point_center)
+                trajectories_dict[tid] = trajectories_dict[tid][-30:]  # keep 30 points
+            else:
+                trajectories_dict[tid] = [point_center]
+            trajectories = trajectories_dict[tid]
+            for trajectory in trajectories:
+                cv2.circle(frame, trajectory, 2, color_id, -1)
             info = 'tid' + ' %d' % sort_id
-            fontScale = 1.2
-            cv2.putText(frame, info,
-                        (box[0], box[1] + int(fontScale * 25)), cv2.FONT_HERSHEY_SIMPLEX, fontScale, color_id,
-                        2)
+            cv2.putText(frame, info, (box[0], box[1] + int(1.2 * 25)), cv2.FONT_HERSHEY_SIMPLEX, 1.2, color_id, 2)
         end_time = time.time()
         frame_count += 1
         elapsed_time = end_time - start_time
         fps = frame_count / elapsed_time
-        cv2.putText(frame, f'FPS: {fps:.2f}', (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-        cv2.putText(frame, f'{idx_frame}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, 1, 2)
+        cv2.putText(frame, f'FPS: {fps:.2f}', (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2)
+        cv2.putText(frame, f'id: {idx_frame}', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
         cv2.imshow(name_window, frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             event.set()
